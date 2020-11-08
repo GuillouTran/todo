e5d3dc0f1a7a296d488e6fe37668395b22d2ef07
Restyled.io <commits@restyled.io>
2020-11-08 14:38:51 +0000

    Restyled by prettier

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey : "AIzaSyAXSBUEREBmqSxolYxZDzjg9dzj98rprU0",
-  authDomain : "guilloutran-book.firebaseapp.com",
-  databaseURL : "https://guilloutran-book.firebaseio.com",
-  projectId : "guilloutran-book",
-  storageBucket : "guilloutran-book.appspot.com",
-  messagingSenderId : "25555383907",
-  appId : "1:25555383907:web:cf76833bfe14c6059faa56",
-  measurementId : "G-LCMSSKY1WG",
+  apiKey: "AIzaSyAXSBUEREBmqSxolYxZDzjg9dzj98rprU0",
+  authDomain: "guilloutran-book.firebaseapp.com",
+  databaseURL: "https://guilloutran-book.firebaseio.com",
+  projectId: "guilloutran-book",
+  storageBucket: "guilloutran-book.appspot.com",
+  messagingSenderId: "25555383907",
+  appId: "1:25555383907:web:cf76833bfe14c6059faa56",
+  measurementId: "G-LCMSSKY1WG",
 };
 
 const firebaseConf = firebase.initializeApp(config);
